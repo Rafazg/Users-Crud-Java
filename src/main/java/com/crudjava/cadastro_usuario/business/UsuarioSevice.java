cc00a303f956df67d6e0df61aa4dc3d13a237c92
Rafazg <claudenirorafaelbjj@gmail.com>
2025-09-29 18:12:51 -0300

    adding: Crud project with SpringBoot

---
+++ src/main/java/com/crudjava/cadastro_usuario/business/UsuarioSevice.java
@@ -0,0 +1,41 @@
+package com.crudjava.cadastro_usuario.business;
+
+import com.crudjava.cadastro_usuario.infrastructure.entitys.Usuario;
+import com.crudjava.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioSevice {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioSevice(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarUsuario(Usuario usuario) {
+        repository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return repository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email) {
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
+    }
+}
---
+++ src/main/java/com/crudjava/cadastro_usuario/infrastructure/entitys/Usuario.java
@@ -0,0 +1,23 @@
+package com.crudjava.cadastro_usuario.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+public final class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+}
